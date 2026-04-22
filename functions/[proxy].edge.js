ad6cbbfcb6718405a7fc870066978e68b1948f88
SakshiKoli-CS <sakshi.koli@contentstack.com>
2026-04-22 10:05:06 +0530

    remove edge function

--- functions/[proxy].edge.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(request, context) {
-  // Simple response to test if the edge function is being hit
-  return new Response("Edge function is working! (No logic)", {
-    status: 200,
-    headers: {
-      "content-type": "text/plain",
-    },
-  });
-}
